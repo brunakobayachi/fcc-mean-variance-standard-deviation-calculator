dac999275686e69a07d450e5162a85af6b628769
Bruna Kobayachi <80274967+brunakobayachi@users.noreply.github.com>
2022-05-24 15:02:39 -0300

    Update mean_var_std.py

--- boilerplate-mean-variance-standard-deviation-calculator-1/mean_var_std.py
+++ boilerplate-mean-variance-standard-deviation-calculator-1/mean_var_std.py
@@ -13,25 +13,25 @@ def calculate(list):
     array.mean(axis = 0).tolist(),
     array.mean(axis = 1).tolist(),
     array.mean()
-  ],
+  ]
     
   calculations['variance'] = [
     array.var(axis = 0).tolist(),
     array.var(axis = 1).tolist(),
     array.var()
-  ],
+  ]
     
   calculations['standard deviation'] = [
     array.std(axis = 0).tolist(),
     array.std(axis = 1).tolist(),
     array.std()
-  ],
+  ]
     
   calculations['max'] = [
     array.max(axis = 0).tolist(),
     array.max(axis = 1).tolist(),
     array.max()
-  ],
+  ]
     
   calculations["min"] = [
     array.min(axis = 0).tolist(),
